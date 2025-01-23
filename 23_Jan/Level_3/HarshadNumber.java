72eac4059ed7c0c14dd94b27561470688d9ccbd5
Anshu Pathak <surajshiv15ss@gmail.com>
2025-01-23 15:33:15 +0530

    Level_3 Complete

---
+++ 23_Jan/Level_3/HarshadNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int originalNumber = number, sum = 0;
+
+        while (originalNumber != 0) {
+            sum += originalNumber % 10;
+            originalNumber /= 10;
+        }
+
+        if (number % sum == 0) {
+            System.out.println(number + " is a Harshad number.");
+        } else {
+            System.out.println(number + " is not a Harshad number.");
+        }
+    }
+}
