e6a1b9835d39d648a53b8db7afe2f0b0bdfe5cf6
Anshu Pathak <surajshiv15ss@gmail.com>
2025-01-23 11:18:22 +0530

    Level 1 Complete

---
+++ 23_Jan/Level_1/NaturalSumWhile.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NaturalSumWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        if (n > 0) {
+            int sumFormula = n * (n + 1) / 2;
+            int sumLoop = 0, i = 1;
+            while (i <= n) {
+                sumLoop += i;
+                i++;
+            }
+            System.out.println("Sum using formula: " + sumFormula);
+            System.out.println("Sum using loop: " + sumLoop);
+        } else {
+            System.out.println("Not a natural number");
+        }
+    }
+}
